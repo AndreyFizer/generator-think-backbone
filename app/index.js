f0461a69feb01c3460f3bb1ca7951685ad8a6f4c
AndreyFizer <fizer.ap@outlook.com>
2016-05-12 22:28:40 +0300

    first push

--- app/index.js
+++ app/index.js
@@ -11,21 +11,21 @@ var _ = require('underscore.string');
 var AppGenerator = module.exports = function Appgenerator(args, options, config) {
     yeoman.generators.Base.apply(this, arguments);
 
-    this.testFramework = this.options['test-framework'] || 'mocha';
+    //this.testFramework = this.options['test-framework'] || 'mocha';
     this.templateFramework = this.options['template-framework'] || 'lodash';
-    this.option('coffee', { desc: 'CoffeeScript instead standard JavaScript' });
+    //this.option('coffee', { desc: 'CoffeeScript instead standard JavaScript' });
 
     // for hooks to resolve on mocha by default
-    if (!this.options['test-framework']) {
-        this.options['test-framework'] = 'mocha';
-    }
+    // if (!this.options['test-framework']) {
+    //     this.options['test-framework'] = 'mocha';
+    // }
 
     // resolved to mocha by default (could be switched to jasmine for instance)
-    this.hookFor('test-framework', { as: 'app' });
-
-    var ext = this.options.coffee ? 'coffee' : 'js';
+    // this.hookFor('test-framework', { as: 'app' });
+    
+    //var ext = 'js';
     this.indexFile = this.readFileAsString(path.join(this.sourceRoot(), 'index.html'));
-    this.mainJsFile = this.readFileAsString(path.join(this.sourceRoot(), 'main.' + ext));
+    this.mainJsFile = this.readFileAsString(path.join(this.sourceRoot(), 'main.js'));
 
     this.on('end', function () {
         this.installDependencies({
@@ -81,21 +81,13 @@ AppGenerator.prototype.askFor = function() {
         var features = answers.features;
 
         function hasFeature(feat) { return features.indexOf(feat) !== -1; }
-
-        // manually deal with the response, get back and store the results.
-        // we change a bit this way of doing to automatically do this in the self.prompt() method.
+        
         this.bootstrapFlavor = answers.bootstrapFlavor;
         this.includeRequireJS = hasFeature('includeRequireJS');
-        //this.includeModernizr = hasFeature('includeModernizr');
-        //this.coffee = !this.options.coffee ? hasFeature('coffee') : this.options.coffee;
 
         cb();
     }.bind(this));
 };
-
-// AppGenerator.prototype.gruntfile = function gruntfile() {
-//     this.template('Gruntfile.js');
-// };
 
 AppGenerator.prototype.packageJSON = function packageJSON() {
     this.template('_package.json', 'package.json');
@@ -103,7 +95,6 @@ AppGenerator.prototype.packageJSON = function packageJSON() {
 
 AppGenerator.prototype.git = function git() {
     this.copy('gitignore', '.gitignore');
-    //this.copy('gitattributes', '.gitattributes');
 };
 
 AppGenerator.prototype.bower = function() {
@@ -111,81 +102,9 @@ AppGenerator.prototype.bower = function() {
     this.copy('_bower.json', 'bower.json');
 };
 
-// AppGenerator.prototype.jshint = function jshint() {
-//     this.copy('jshintrc', '.jshintrc');
-// };
-
-// AppGenerator.prototype.editorConfig = function editorConfig() {
-//     this.copy('editorconfig', '.editorconfig');
-// };
-
-// AppGenerator.prototype.h5bp = function h5bp() {
-//     this.copy('favicon.ico', 'app/favicon.ico');
-//     this.copy('404.html', 'app/404.html');
-//     this.copy('robots.txt', 'app/robots.txt');
-//     this.copy('htaccess', 'app/.htaccess');
-// };
-
 AppGenerator.prototype.mainStylesheet = function mainStylesheet() {
-    // if (this.bootstrapFlavor === 'sass') {
-    //     this.copy('main.scss', 'app/styles/main.scss');
-    // } else if (this.bootstrapFlavor === 'less') {
-    //     this.copy('main.less', 'app/styles/main.less');
-    // } else {
-    //     this.copy('main.css', 'app/styles/main.css');
-    // }
     this.copy('main.css', 'public/styles/main.css');
 };
-
-// AppGenerator.prototype.writeIndex = function writeIndex() {
-//     if (this.includeRequireJS) {
-//         return;
-//     }
-//
-//     this.indexFile = this.readFileAsString(path.join(this.sourceRoot(), 'index.html'));
-//     this.indexFile = this.engine(this.indexFile, this);
-//
-//     var vendorJS = [
-//         'bower_components/jquery/jquery.js',
-//         'bower_components/underscore/underscore.js',
-//         'bower_components/backbone/backbone.js'
-//     ];
-//
-//     if (this.templateFramework === 'handlebars') {
-//         vendorJS.push('bower_components/handlebars/handlebars.js');
-//     }
-//
-//     this.indexFile = this.appendScripts(this.indexFile, 'scripts/vendor.js', vendorJS);
-//
-//     if (this.coffee) {
-//         this.indexFile = this.appendFiles({
-//             html: this.indexFile,
-//             fileType: 'js',
-//             optimizedPath: 'scripts/coffee.js',
-//             sourceFileList: ['scripts/hello.js'],
-//             searchPath: '.tmp'
-//         });
-//     }
-//
-//     // wire Bootstrap plugins
-//     if (this.bootstrapFlavor !== 'none') {
-//         var dirName = this.bootstrapFlavor == 'sass' ? 'sass-bootstrap' : 'bootstrap'
-//         this.indexFile = this.appendScripts(this.indexFile, 'scripts/plugins.js', [
-//             'bower_components/' + dirName + '/js/affix.js',
-//             'bower_components/' + dirName + '/js/alert.js',
-//             'bower_components/' + dirName + '/js/dropdown.js',
-//             'bower_components/' + dirName + '/js/tooltip.js',
-//             'bower_components/' + dirName + '/js/modal.js',
-//             'bower_components/' + dirName + '/js/transition.js',
-//             'bower_components/' + dirName + '/js/button.js',
-//             'bower_components/' + dirName + '/js/popover.js',
-//             'bower_components/' + dirName + '/js/carousel.js',
-//             'bower_components/' + dirName + '/js/scrollspy.js',
-//             'bower_components/' + dirName + '/js/collapse.js',
-//             'bower_components/' + dirName + '/js/tab.js'
-//         ]);
-//     }
-// };
 
 AppGenerator.prototype.writeIndexWithRequirejs = function writeIndexWithRequirejs() {
     if (!this.includeRequireJS) {
@@ -209,12 +128,6 @@ AppGenerator.prototype.app = function app() {
     this.write('public/index.html', this.indexFile);
 };
 
-// AppGenerator.prototype.bootstrapJS = function bootstrapJS() {
-//     // TODO: create a Bower component for this
-//     if (this.includeRequireJS && this.bootstrapFlavor !== 'none') {
-//         this.copy('bootstrap.js', 'app/scripts/vendor/bootstrap.js');
-//     }
-// };
 
 AppGenerator.prototype.mainJS = function mainJS() {
     if (!this.includeRequireJS) {
@@ -222,20 +135,8 @@ AppGenerator.prototype.mainJS = function mainJS() {
     }
     var dirPath = '../templates';
     this.sourceRoot(path.join(__dirname, dirPath));
+    
+    var mainJsFile = this.engine(this.read('requirejs_app.js'), this);
 
-    var ext = 'js';
-    var mainJsFile = this.engine(this.read('requirejs_app.' + ext), this);
-
-    this.write('public/js/main.' + ext, mainJsFile);
+    this.write('public/js/main.js', mainJsFile);
 };
-
-// AppGenerator.prototype.createAppFile = function createAppFile() {
-//     if (this.includeRequireJS) {
-//         return;
-//     }
-//     var dirPath = this.options.coffee ? '../templates/coffeescript/' : '../templates';
-//     this.sourceRoot(path.join(__dirname, dirPath));
-//
-//     var ext = this.coffee ? 'coffee' : 'js';
-//     this.template('app.' + ext, 'app/scripts/main.' + ext);
-// };
---
+++ app/templates/main.js
@@ -0,0 +1,7 @@
+/**
+ * Created by andrey on 12.05.16.
+ */
+
+define(['<%= _.slugify(appname) %>', 'jquery'], function(<%= _.classify(appname) %>, $){
+<%= _.classify(appname) %>.start();
+});
